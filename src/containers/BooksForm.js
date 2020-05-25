5536975fa636b726b6023ff4ae7a251d5b0fa6ba
Fabricio Garcia <gael.fabricio@gmail.com>
2020-05-25 16:00:16 -0500

    feat: Implement BooksForm component

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Book from '../components/Book';
 
 const BooksForm = ({ books }) => (
   <div className="BooksForm">
-    
+
+  <form>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputTitle">Title</label>
+        <input type="text" class="form-control" id="inputTitle" />
+      </div>
+      <div class="form-group col-md-4">
+        <label for="inputCategory">Category</label>
+        <select id="inputCategory" class="form-control">
+          <option>Action</option>
+          <option>Biography</option>
+          <option>History</option>
+          <option>Horror</option>
+          <option>Kids</option>
+          <option>Learning</option>
+          <option>Sci-Fi</option>
+        </select>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
   </div>
 );
 
-
-
-
-export default connect(mapStateToProps)(BooksForm);
+export default BooksForm;
